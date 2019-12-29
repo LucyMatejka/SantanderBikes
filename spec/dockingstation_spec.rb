eafcb6b5738e4958773fa653358ffab3392ffe45
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 13:40:57 +0000

    tests for docking_station and bike

---
+++ spec/dockingstation_spec.rb
@@ -0,0 +1,7 @@
+require 'dockingstation'
+
+describe DockingStation do
+  #  it 'responds to release_bike' do
+  #    expect(subject).to respond_to :release_bike
+  it { is_expected.to respond_to :release_bike }
+end
