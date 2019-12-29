bd676e425e64f16a26bfd123fd3cb7b584b2f4d9
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 17:08:24 +0000

    refactor bike method

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,6 +1,8 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+#replaces bike method to return @bike (the docked bike)
   def release_bike
     Bike.new
   end
@@ -10,8 +12,8 @@ class DockingStation
   end
 #instance variable used to store the bike
 
-  def bike
-    @bike
-  end
+#  def bike
+#    @bike
+#  end
 #return the docked bike
 end
