8fb8c32f2cd0a8de9a1447098d3a034a91d692f3
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 18:56:25 +0000

    raise an error if docking_station full

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -27,6 +27,13 @@ it 'docks something' do
   expect(subject.dock(bike)).to eq bike
 end
 
+describe '#dock' do
+  it 'raises an error when full' do
+    subject.dock(Bike.new)
+    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+  end
+end
+
 it {is_expected.to respond_to(:bike)}
 it 'returns docked bikes' do
   bike = Bike.new
