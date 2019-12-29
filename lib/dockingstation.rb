153225c6f1b02bdba56eebff6d3c69e370afb408
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 19:23:19 +0000

    instance variable array for bikes

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,15 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @bikes = []
+  end
+  #create array for bikes
   attr_reader :bike
 #replaces bike method to return @bike (the docked bike)
   def release_bike
-    Bike.new
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+    #Bike.new
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
   end
 #instance variable used to store the bike
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -29,9 +29,10 @@ end
 
 describe '#dock' do
   it 'raises an error when full' do
-    subject.dock(Bike.new)
+    20.times {subject.dock(Bike.new)}
     expect { subject.dock Bike.new }.to raise_error 'Docking station full'
   end
+  #updated for 7th user story- docking station can dock 20 bikes
 end
 
 it {is_expected.to respond_to(:bike)}
