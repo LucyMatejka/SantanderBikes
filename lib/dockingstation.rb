141b914f21dfbdbe536e0234149ae322b97cf376
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 19:54:43 +0000

    private methods for full and empty

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -8,14 +8,28 @@ class DockingStation
   attr_reader :bike
 #replaces bike method to return @bike (the docked bike)
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    #fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
+    #use private helper method empty? Single Responsibility Principle
     @bikes.pop
     #Bike.new
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    #fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
+    #use private method helper full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 #instance variable used to store the bike
 
