4e3a7fa7fd4ef157f6852d9a1f98aaf4519b892d
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 16:47:22 +0000

    create instance variable for bike

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -6,6 +6,12 @@ class DockingStation
   end
 
   def dock(bike)
+    @bike = bike
   end
+#instance variable used to store the bike
 
+  def bike
+    @bike
+  end
+#return the docked bike
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -10,4 +10,15 @@ describe DockingStation do
   end
 
   it {is_expected.to respond_to(:dock).with(1).arguments }
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it {is_expected.to respond_to(:bike)}
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
