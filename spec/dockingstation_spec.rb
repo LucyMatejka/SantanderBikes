ebeaa8a9c203bebb6eb50367aee59ae421e71199
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-12-29 14:32:36 +0000

    passing unit tests for bike and dockingstation

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
   #  it 'responds to release_bike' do
   #    expect(subject).to respond_to :release_bike
   it { is_expected.to respond_to :release_bike }
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
