4c565ddfa323f5fdda64a87dc065ff763a516a30
Lucy Matejka <lucymatejka@yahoo.co.uk>
2020-01-13 21:39:47 +0000

    final commit

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,7 +5,9 @@ describe DockingStation do
   #  it 'responds to release_bike' do
   #    expect(subject).to respond_to :release_bike
   it { is_expected.to respond_to :release_bike }
+
   it 'releases working bikes' do
+    subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -31,16 +33,16 @@ describe DockingStation do
   end
 
 
-  describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
-    it 'raises an error when there are no bikes available' do
-      expect {subject.release_bike }.to raise_error 'No bikes available'
-    end
-  end
+#  describe '#release_bike' do
+#    it 'releases a bike' do
+#      bike = Bike.new
+#      subject.dock(bike)
+#      expect(subject.release_bike).to eq bike
+#    end
+#    it 'raises an error when there are no bikes available' do
+#      expect {subject.release_bike }.to raise_error 'No bikes available'
+#    end
+#  end
 
 it {is_expected.to respond_to(:dock).with(1).arguments }
 it 'docks something' do
@@ -48,16 +50,15 @@ it 'docks something' do
   expect(subject.dock(bike)).to eq bike
 end
 
-describe '#dock' do
+describe 'dock' do
   it 'raises an error when full' do
     #20.times {subject.dock(Bike.new)}
-    DockingStation::DEFAULT_CAPACITY.times do
+  #  DockingStation::DEFAULT_CAPACITY.times do
       subject.capacity.times { subject.dock Bike.new }
     end
     expect { subject.dock Bike.new }.to raise_error 'Docking station full'
   end
   #updated for 7th user story- docking station can dock 20 bikes
-end
 
 it {is_expected.to respond_to(:bike)}
 it 'returns docked bikes' do
